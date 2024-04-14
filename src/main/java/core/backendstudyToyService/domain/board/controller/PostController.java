a6fc305f35a6ec553c5b3865ed7f0fd0f0bb15ad
heoap <heoap9@gmail.com>
2024-04-15 05:47:21 +0900

    Post mapping on likes and comments but Session not onboard

--- src/main/java/core/backendstudyToyService/domain/board/controller/PostController.java
+++ src/main/java/core/backendstudyToyService/domain/board/controller/PostController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -18,10 +19,17 @@ public class PostController {
 //메롱
     private PostService postService;
 
+    /**
+     * 게시글에 포함된 댓글목록,좋아요 수를 같이 반환합니다
+     * @param postId 게시글 번호가 들어옵니다
+     * @param model 대빵많이 해야되니까 모델이 필요해요
+     * @return 게시글 번호를 등록하면 디테일폼으로 반환합니다
+     */
     @GetMapping("/posts/{postId}")
     public String getPostDetails(@PathVariable Long postId, Model model){
         try {
             PostDetailsDTO postDetailsDTO = postService.getPostDetails(postId);
+            //이미지 프로세싱은 보류
             model.addAttribute("postDetails",postDetailsDTO);
             return "post-details";
         }catch (RuntimeException e){
@@ -29,4 +37,31 @@ public class PostController {
             return "error";
         }
     }
+
+    /**
+     * 게시글에 좋아요를 등록합니다
+     * 유저 세션정보에 따라 좋아요 결과가 반영됩니다
+     * @param postId 게시글 번호
+     * @return 리다이렉션 결과를 반환합니다
+     */
+    @PostMapping("/posts/{postId}/likes")
+    public String addLikePost(@PathVariable Long postId){
+        Long userId = 1L;
+        postService.addLikePost(postId,userId);
+        return "redirect:/posts/"+postId;
+    }//세션정보가 추가되면 파라미터에 추가해주세요!
+
+    /**
+     * 게시글 댓글을 등록합니다
+     * 유저 세션정보가 있으면 작성자도 포함됩니다
+     * @param postId 게시글번호
+     * @param content 댓글내용
+     * @return 리다이렉션 결과를 반환합니다
+     */
+    @PostMapping("/posts/{postId}/comments")
+    public String addComment(@PathVariable Long postId,@RequestParam String content){
+        Long userId = 1L;
+        postService.addCommentPost(postId,userId,content);
+        return "redirect:/posts/"+postId;
+    }//세션정보가 추가되면 파라미터에 추가해주세요!
 }
---
+++ src/test/java/core/backendstudyToyService/AddingDummyData.java
@@ -0,0 +1,16 @@
+package core.backendstudyToyService;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class AddingDummyData {
+    @Test
+    @DisplayName("대빵많은 더미데이터입니다")
+    //사용시에 application yaml파일을 수정해주세요
+    public void injectData(){
+
+        for(int i = 0; i< 100;i++){
+
+        }
+    }
+}
